3ee028736b95acc9a1c689320a4a87b5f1ce4ae8
alternatief1983 <alternatief1983@gmail.com>
2026-03-16 23:01:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -23,6 +23,21 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
